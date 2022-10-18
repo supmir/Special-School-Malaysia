e21636210b341200a329a046578488af47c182a6
Amir <iskandar.amir@gmail.com>
2022-10-18 18:32:34 +0800

    Front page complete

---
+++ components/layouts/default.js
@@ -0,0 +1,4 @@
+export default function Default(props) {
+  const { children } = props;
+  return <div className="max-w-xl mx-auto px-2">{children}</div>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,34 @@
+import Link from "next/link";
+import Default from "../components/layouts/default";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const categories = [
+    { href: "hearingImpaired", text: "Hearing Impaired" },
+    { href: "visuallyImpaired", text: "Visually impaired" },
+    { href: "slowLearner", text: "Slow Learner" },
+    { href: "dyslexic", text: "Dyslexic" },
+    { href: "ADHD", text: "ADHD" },
+  ];
+
+  return (
+    <Default>
+      <div>
+        <div className="text-xl text-center">
+          School for Student with Learning Disability
+        </div>
+
+        <div className="flex justify-around flex-wrap gap-y-2 my-10">
+          {categories.map(({ href, text }) => (
+            <div className="w-full sm:w-1/2 md:w-1/3 px-2">
+              <Link href={`/categories/${href}`}>
+                <div className="text-center border border-black rounded-xl">
+                  {text}
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Default>
+  );
 }
