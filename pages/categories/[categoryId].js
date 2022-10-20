a813fe6b5c0f913c7123b708dca63bb5686f0788
Amir <iskandar.amir@gmail.com>
2022-10-20 17:33:31 +0800

    Make category page

---
+++ pages/categories/[categoryId].js
@@ -0,0 +1,31 @@
+import { categories, schoolByCat } from "../../site/data";
+
+export default function Category(props) {
+  const { schools } = props;
+
+  return (
+    <div>
+      {schools.map((schoolInfo) => (
+        <div>{schoolInfo["Name of School"]}</div>
+      ))}
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: categories.map(({ categoryId }) => ({
+      params: { categoryId: categoryId },
+    })),
+    fallback: false, // can also be true or 'blocking'
+  };
+}
+export async function getStaticProps(context) {
+  const { categoryId } = context.params;
+  console.log(schoolByCat[categoryId]);
+  return {
+    props: {
+      schools: schoolByCat[categoryId],
+    },
+  };
+}
